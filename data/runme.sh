e09f515ef9fb9fe837d726443424051f48fef6a3
Vitali.Adamenka <Vitali.Adamenko@promwad.com>
2018-11-01 16:00:17 +0300

    Add default valiables for path and version

--- data/runme.sh
+++ data/runme.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 arr=(`ls | grep -e '^qt-unified[[:graph:]]*run$' | sort -f`)
 position=$(( ${#arr[*]} - 1 ))
+
+if [ -z $DESTINATION ]; then
+    DESTINATION=/opt/Qt
+fi
+
+if [ -z $QT_COMPONENTS ]; then
+    QT_COMPONENTS='qt.qt5.5112.gcc_64'
+fi
+
 ./${arr[$position]} -v --script script.qs DESTINATION=$DESTINATION COMPONENTS=$QT_COMPONENTS 
 
 # TODO select appropriate Qt
 #export QT_SELECT=$DESTINATION
+# TODO build openssl
